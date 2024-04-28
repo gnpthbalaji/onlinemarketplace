7c0584efa0a1456242ab69031ec5ddbfa0334cd4
Balaji Guntur <59932973+gnpthbalaji@users.noreply.github.com>
2024-04-28 01:06:54 -0700

    Updating Settings for whitenoise

--- onlinemarketplace/settings.py
+++ onlinemarketplace/settings.py
@@ -122,7 +122,11 @@ STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    'staticfiles': {
+        "BACKEND":'django.core.files.storage.FileSystemStorage',
+    }
+}
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
