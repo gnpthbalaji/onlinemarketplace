42023e88da6c9de50f67e83ca27138c8c11b6081
Balaji Guntur <59932973+gnpthbalaji@users.noreply.github.com>
2024-04-28 01:09:54 -0700

    Update STATICFILES_STORAGE setting to use 'whitenoise.storage.CompressedManifestStaticFilesStorage'

--- onlinemarketplace/settings.py
+++ onlinemarketplace/settings.py
@@ -126,6 +126,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 MEDIA_URL = '/images/'
 
