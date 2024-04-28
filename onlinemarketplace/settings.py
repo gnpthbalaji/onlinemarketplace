cb41276e2162b1fa0e1ad0f62af06f7a3ec51e4a
Balaji Guntur <59932973+gnpthbalaji@users.noreply.github.com>
2024-04-28 01:05:34 -0700

    Updating Settings for Static files

--- onlinemarketplace/settings.py
+++ onlinemarketplace/settings.py
@@ -122,6 +122,8 @@ STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
